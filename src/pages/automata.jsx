dfb927ce21e40a069945ddaf87bc002ad93f77ac
Cameron Blandford <cam.blandford@gmail.com>
2019-10-04 00:28:17 -0400

    Add beginnings of automata

---
+++ src/pages/automata.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import CellMap from "../components/pieces/automata"
+import Layout from "../components/layout"
+export default function automata() {
+  return (
+    <Layout>
+      <CellMap />
+    </Layout>
+  )
+}
